4e15f615d5eb111517857e1379c122c9e9f09ea5
경민 <rodrng@gmail.com>
2022-04-13 18:55:52 +0900

    Xml 설정 파일을 이용하는 경우

---
+++ spring_8_3_ex1_springex/src/main/java/com/javagyeongmin/ex/MainClass.java
@@ -0,0 +1,37 @@
+package com.javagyeongmin.ex;
+
+import java.util.Scanner;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("서버종류를 입력해주세요(dev, run):");
+		Scanner scanner = new Scanner(System.in);
+		
+		String config = null;
+		String str = scanner.next();
+		
+		if(str.equals("dev")) {
+			config = "dev";
+		} else {
+			config = "run";
+		}
+		
+		scanner.close();
+		
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.getEnvironment().setActiveProfiles(config);
+		ctx.load("applicationCTX_dev.xml","applicationCTX_run.xml");
+		
+		ServerInfo s_info = ctx.getBean("serverInfo",ServerInfo.class);
+		System.out.println("ip:" + s_info.getIpNum());
+		System.out.println("port:" + s_info.getPortNum());
+		
+		ctx.close();
+		
+	}
+
+}
